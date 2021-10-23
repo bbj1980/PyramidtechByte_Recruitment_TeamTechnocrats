d6b9bb7e0ca6cf19a806e37f575667a8dc3c91e7
Narender1903 <narender1903@gmail.com>
2021-10-23 14:38:42 +0530

    first checkin

---
+++ WebApi/JobByte/WebApi/WebApi/Controllers/JobDescriptionController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApi.Core;
+using WebApi.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class JobDescriptionController : ControllerBase
+    {
+
+        private readonly ILogger<JobDescriptionController> _logger;
+        private readonly ProcessDocument _processDocument;
+
+        public JobDescriptionController(ILogger<JobDescriptionController> logger)
+        {
+            _logger = logger; _processDocument = new ProcessDocument();
+        }
+        // GET: api/<JobDescriptionController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<JobDescriptionController>/5
+        //[HttpGet("{id}")]
+        //public JobDescription Get(int id)
+        //{
+        //    return this._processDocument.ExtractJobDescription();
+        //}
+
+        // POST api/<JobDescriptionController>
+        [HttpPost]
+        public JobDescription Post([FromBody] string base64encodedstring)
+        {
+            return this._processDocument.ExtractJobDescription(base64encodedstring);
+
+        }
+
+        // PUT api/<JobDescriptionController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<JobDescriptionController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ WebApi/JobByte/WebApi/WebApi/Core/ProcessDocument.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApi.Models;
+using System.IO;
+using System.IO.Packaging;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+using DocumentFormat.OpenXml;
+using System.Net.Http;
+
+namespace WebApi.Core
+{
+    public class ProcessDocument
+    {
+        public JobDescription ExtractJobDescription(string base64encodedstring)
+        {
+            //string filepath = "D:\\temp\\Automation QA1.docx";
+            var bytes = Convert.FromBase64String(base64encodedstring);
+            var contents = new MemoryStream(bytes);
+            var JD = new JobDescription();
+            var tableRows = new List<TableRow>();
+
+            using (WordprocessingDocument wordDocument =
+            WordprocessingDocument.Open(contents, false))
+            {
+                // Assign a reference to the existing document body.  
+
+                tableRows = wordDocument.MainDocumentPart.Document.Body.OfType<Table>().First().OfType<TableRow>().ToList();
+
+                //Extract  Description 
+                JD.Description = tableRows.ElementAt(3).OfType<TableCell>().ElementAt(0).InnerText.Trim();
+                //Extract  Position 
+                JD.Position = tableRows.ElementAt(1).OfType<TableCell>().ElementAt(0).InnerText.Trim();
+                //Extract  Techskill 
+                JD.Techskill = tableRows.ElementAt(5).OfType<TableCell>().ElementAt(0).InnerText.Trim() 
+                    + tableRows.ElementAt(7).OfType<TableCell>().ElementAt(0).InnerText.Trim();
+                //Extract  Softskill 
+                JD.Softskill = tableRows.ElementAt(13).OfType<TableCell>().ElementAt(0).InnerText.Trim();
+                //Extract  Education 
+                JD.Education = tableRows.ElementAt(9).OfType<TableCell>().ElementAt(0).InnerText.Trim();
+                //Extract  Experience 
+                JD.Experience = tableRows.ElementAt(9).OfType<TableCell>().ElementAt(1).InnerText.Trim();
+                //Extract  Comments 
+                JD.Comments = tableRows.ElementAt(15).OfType<TableCell>().ElementAt(0).InnerText;
+
+            }
+
+            return JD;
+        }
+    }
+}
